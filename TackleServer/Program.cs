18f3adeb5914225c47a829f73a85d35f13e6efed
MJGHD <myles.jon.gordon@gmail.com>
2018-07-25 15:15:46 +0100

    Initial commit

---
+++ TackleServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TackleServer
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 8888);
+            server.Start();
+
+            //Loop that constantly checks whether there's an incoming request
+            Console.WriteLine("Client checking loop started");
+            while (true)
+            {
+                Console.WriteLine("test");
+                if (server.Pending())
+                {
+                    Socket client = server.AcceptSocket();
+                    Console.WriteLine("Client handling thread started");
+
+                    Thread clientReceiveThread = new Thread(() =>
+                    {
+                        byte[] message = new byte[100];
+                        client.Receive(message);
+                        string messageStr = Encoding.Default.GetString(message);
+                        client.Close();
+                    });
+
+                    clientReceiveThread.Start();
+                }
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
