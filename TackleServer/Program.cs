3f5ad98b163e200f6c00682145b7fe31db51a951
MJGHD <myles.jon.gordon@gmail.com>
2018-07-29 16:44:58 +0100

    Sign up is mainly working - getting error messages sometimes but not others? Need to add more handling of bad inputs

--- TackleServer/Program.cs
+++ TackleServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -7,12 +8,13 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Data.SQLite;
 
 namespace TackleServer
 {
     class Program
     {
-        
         static void Main()
         {
             TcpListener server = new TcpListener(IPAddress.Any, 8888);
@@ -22,7 +24,6 @@ namespace TackleServer
             Console.WriteLine("Client checking loop started");
             while (true)
             {
-                Console.WriteLine("test");
                 if (server.Pending())
                 {
                     Socket client = server.AcceptSocket();
@@ -30,16 +31,49 @@ namespace TackleServer
 
                     Thread clientReceiveThread = new Thread(() =>
                     {
-                        byte[] message = new byte[100];
+                        byte[] message = new byte[1024];
                         client.Receive(message);
-                        string messageStr = Encoding.Default.GetString(message);
-                        client.Close();
+                        string jsonReceived = Encoding.Default.GetString(message);
+                        ServerRequest clientRequest = JsonConvert.DeserializeObject<ServerRequest>(jsonReceived);
+                        HandleRequest(client, clientRequest);
                     });
 
                     clientReceiveThread.Start();
                 }
                 Thread.Sleep(300);
             }
+
+            void HandleRequest(Socket client, ServerRequest clientRequest)
+            {
+                if(clientRequest.requestSource == "SIGNUP")
+                {
+                    string username = clientRequest.requestParameters[0];
+                    string password = clientRequest.requestParameters[1];
+                    int userType = Int32.Parse(clientRequest.requestParameters[2]);
+
+                    SQLiteConnection databaseConnection = new SQLiteConnection("Data Source=TackleDatabase.db;Version=3;");
+                    databaseConnection.Open();
+
+                    //Escapes any apostrophies in the usernames or passwords so that syntax errors with apostrophes can't occur
+                    username = username.Replace("'", "''");
+                    password = password.Replace("'", "''");
+
+                    string SQL = $"INSERT INTO Users (Username,Password,UserType) VALUES ('{username}','{password}',{userType})";
+                    SQLiteCommand command = new SQLiteCommand(SQL, databaseConnection);
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine($"Sign up request handled for user '{username}' at {client.RemoteEndPoint}");
+                    byte[] response = new byte[] {1};
+                    client.Send(response);
+                    client.Close();
+                }
+            }
+        }
+
+        class ServerRequest
+        {
+            public string requestSource;
+            public string[] requestParameters;
         }
     }
 }
